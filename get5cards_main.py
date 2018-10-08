e19b5d2efc5c1c61a9ae2ebd9647726c8acbf14c
Кирилл Королев <43026761+Abigile@users.noreply.github.com>
2018-10-08 18:09:25 +0500

    Ready function for selecting the last 5 cards
    запускаем get5cards_main.py. Он  запрашивает название скилла у пользователя(Для бота это конечно не подойдет, но зато видно кк каким точкам прикреплять функционал бота), обращается к skillbase.db запрашивая из нее ссылку на страницу со списком работ. Если в базе указанного скилла нет, то выводит список скиллов у которых есть указанные пользователем слова в названии.
    После через get5cards.py, прикрываясь прокси, получает целевую страницу.
    Через get5cards_find_works.py парсит страницу и возвращает в get5cards_main.py 5 или меньше(если их меньше 5) последних записей.
    get5cards_main.py выводит принтом данные всех полученных карточек.

---
+++ get5cards_main.py
@@ -0,0 +1,37 @@
+"""
+Запрашиваем у skillbase.db ссылку на указанный юзером навык
+Получаем 5 словарей с данными 5-ти первых карточек на странице
+Пока принтим их в консоль в человекочитаемом виде
+"""
+
+import sqlalchemy
+
+from sql import db_session, Skillbase
+
+from get5cards import get_5_cards
+
+
+u = Skillbase
+
+while True:
+	user_input = input('Укажите навык по которому искать заказы:\n')
+	try:
+		q = u.query.filter(Skillbase.skill == user_input).first()
+		link = q.link
+		print(link)
+		break
+	except AttributeError:
+		print('Такого навыка в базе нет')
+		user_input = "%" + user_input + "%"
+		q_user = u.query.filter(Skillbase.skill_words.like(user_input)).all()
+		print("Может быть вы имели ввиду:\n")
+		for skil in q_user:
+			print(skil.skill)
+
+cards = get_5_cards(link)
+for card in cards:
+	if card['verified'] == True:
+		pay_metod = '\tСпособ оплаты подтвержден'
+	else: 
+		pay_metod = ''
+	print('\nЗадание: {0}\nОплата:{5}{6}\tАктивно еще {1}.\n\nОписание: {2}\n\nТребуемые навыки{3}\n\nПредложений от фрилансеров:{7}\n\nСтраница заказа:  {4}\n\n'.format(card['title'],card['time'],card['description'],card['list_skill'],card['link'],card['price'],pay_metod,card['bids']))
